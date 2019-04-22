9741ae3ed74d11091fc0c6e8f023ce807543cb1b
damo <1902065822@qq.com>
2019-04-22 14:28:45 +0800

    修改api

--- lib/module/douban/utils/const_douban.dart
+++ lib/module/douban/utils/const_douban.dart
@@ -3,13 +3,13 @@ class ApiDouban {
 
   ///上映
   static String getMovieInTheaters(int start,int count) =>
-      'https://api.module.douban.com/v2/movie/in_theaters?apikey=$API_KEY&city=上海&start=$start&count=$count';
+      'https://api.douban.com/v2/movie/in_theaters?apikey=$API_KEY&city=上海&start=$start&count=$count';
   static const Movie_In_Theaters_Subject =
-      'http://api.module.douban.com/v2/movie/subject/';
+      'http://api.douban.com/v2/movie/subject/';
 
   static String getMovieComment(String id,int count) =>
-      'https://api.module.douban.com/v2/movie/subject/$id/comments?apikey=$API_KEY&count=$count';
+      'https://api.douban.com/v2/movie/subject/$id/comments?apikey=$API_KEY&count=$count';
 
   static String getMoviePhotos(String id,int start,int count)=>
-      'https://api.module.douban.com/v2/movie/subject/$id/photos?apikey=$API_KEY&start=$start&count=$count';
+      'https://api.douban.com/v2/movie/subject/$id/photos?apikey=$API_KEY&start=$start&count=$count';
 }
